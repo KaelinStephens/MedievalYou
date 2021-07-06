419b7c9d5a5d13711e491910da81bd519963307d
TheWookersquatch <ksprettygurl@gmail.com>
2021-07-05 19:02:01 -0600

    documentation added

--- MedievalTextGame/Human.cs
+++ MedievalTextGame/Human.cs
@@ -28,7 +28,7 @@ namespace MedievalTextGame
             Pronoun = pronoun;
             Path = path;
             PathMessage = pathMessage;
-            Console.WriteLine($"{FirstName} {LastName} is a {Age} year old Human who identifies as {Gender}. {FirstName} {PathMessage}.");
+            Console.WriteLine($"{FirstName} {LastName} is a {Age} year old Human who identifies as {Gender}. {FirstName} {PathMessage}.");  //character creation message prints when a new character is created.
         }
         public string FirstName{get; set;}
         public string LastName { get; set; }
@@ -36,32 +36,32 @@ namespace MedievalTextGame
         public string Gender { get; set; }
         public string Pronoun { get; set; }
         public string Path { get; set; }
-        public string PathMessage { get; set; }
-        public static Human CreateNewHuman()
+        public string PathMessage { get; set; } //this property is purely for having fun with the character creation message.
+        public static Human CreateNewHuman()   //Human class method called from Main to create a new character.
         {
             Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
+            string firstName = Console.ReadLine();          //takes any input as firstName.
             Console.WriteLine();
-            Console.WriteLine("What is your surname?");
+            Console.WriteLine("What is your surname?");    //takes any input as lastName. 
             string lastName = Console.ReadLine();
             Console.WriteLine();
-            int age = GetAge();
+            int age = GetAge();            //calls GetAge method to confirm input is numbers and within the parameters of the game then returns the int age.
             Console.WriteLine();
-            string gender = GetGender();
+            string gender = GetGender();   //calls GetGender method to populate gender property with a game-specific value.
             Console.WriteLine();
-            string pronoun = GetPronoun(gender);
-            string path = GetPath();
-            string pathMessage = GetPathMessage(path);
+            string pronoun = GetPronoun(gender);  //calls GetPronoun method to set the character's pronouns based on what gender was chosen. If "other" was chosen, also prints additional messages and requests.
+            string path = GetPath();           //calls GetPath method to populate path property with a game-specific value.       
+            string pathMessage = GetPathMessage(path);     //calls GetPathMessage method to set the character's pathMessage based on what path was chosen.
             Console.WriteLine();
 
 
-            return new Human(firstName, lastName, age, gender, pronoun, path, pathMessage);
+            return new Human(firstName, lastName, age, gender, pronoun, path, pathMessage); //actually populates the properties and creates the character.
 
         }
 
-        public static string GetPath()
+        public static string GetPath()          //creates a loop requiring player to choose a provided value then returns the path to CreateNewHuman() method.
         {
-            string path;
+            string path;     
             do
             {
                 Console.WriteLine("What is your destiny?");
@@ -80,7 +80,7 @@ namespace MedievalTextGame
                         return path;
                     case "mage":
                         return path;
-                    default:
+                    default:                                       //if invalid input is entered, message shows and loop begins again until a provided path is chosen.
                         Console.WriteLine($"*wagging my finger* Ah - ah - ah! You didn't say the magic word!");
                         Console.WriteLine($"You must choose one of the prescribed destinies.");
                         Console.WriteLine();
@@ -88,10 +88,10 @@ namespace MedievalTextGame
                 }
             } while (path != "knight" && path != "thief" && path != "druid" && path != "mage");
 
-            return path;
+            return path;  //just here to satisfy the return requirements for the method. This line will never get used. All returns can only come from within the do/while loop.
         }
 
-        public static string GetPathMessage(string path)
+        public static string GetPathMessage(string path)    //creates a switch case for the pathMessage based on the path chosen. Then returns the pathMessage to CreateNewHuman() method.
         {
             string pathMessage;
             switch (path)
@@ -112,9 +112,9 @@ namespace MedievalTextGame
                     return pathMessage;
             }
 
-            return "nothing to see here";
+            return "nothing to see here"; //just here to satisfy the return requirements for the method. This line will never get used. All returns can only come from within the do/while loop.
         }
-        public static string GetGender()
+        public static string GetGender()         //creates a loop requiring player to choose a provided value then returns the gender to CreateNewHuman() method.
         {
             string gender;
             do
@@ -132,60 +132,60 @@ namespace MedievalTextGame
                         return gender;
                     case "other":
                         return gender;
-                    default:
+                    default:                            //if invalid input is entered, message shows and loop begins again until a provided path is chosen.
                         Console.WriteLine();
                         Console.WriteLine($"{gender} is not one of the options presented. Please try again.");
                         Console.WriteLine();
                         break;
                 }
-            } while (gender != "male" && gender != "female" && gender != "other");
+            } while (gender != "male" && gender != "female" && gender != "other");      //just means if 'male', 'female', or 'other' are not the input the loop starts over.
 
-            return gender;
+            return gender;     //just here to satisfy the return requirements for the method. This line will never get used. All returns can only come from within the do/while loop.
 
         }
 
-        public static int GetAge()
-        {
-            int age = IsNumbers();
+        public static int GetAge()           //calls IsNumber method to confirm the input is all numbers. 
+        {                                   //Next creates a loop requiring player to enter a number between 1 and 75. Then returns the age to CreateNewHuman() method.
+            int age = IsNumbers();   //method called to convert input to int. If non-numbers are entered, method requires new input until all numbers are entered.
 
-            while (age < 0 || age > 75)
+            while (age < 0 || age > 75)        //if number is too high or too low, message shows and loop begins again until a provided path is chosen.
             {
-                while (age > 75)
+                while (age > 75)            //if input is over 75, message prints, IsNumbers() method is called again and loop repeats.
                 {
                     Console.WriteLine("Nice try, but no one lives over the age of 75 in the Medieval Times. Don't ask me how I know. I just do.");
                     Console.WriteLine();
-                    age = IsNumbers();
+                    age = IsNumbers();         //method called to convert input to int. If non-numbers are entered, method requires new input until all numbers are entered.
                 }
-                while (age < 0)
+                while (age < 0)          //if input is under 1, message prints, IsNumbers() method is called again and loop repeats.
                 {
                     Console.WriteLine("Hmm...Nope. Your responding to these prompts indicates you must already be born.");
                     Console.WriteLine();
-                    age = IsNumbers();
+                    age = IsNumbers();        //method called to convert input to int. If non-numbers are entered, method requires new input until all numbers are entered.
                 }
             }
-            return age;
+            return age;            //returns age to CreateNewHuman() method.      
         }
 
-        public static int IsNumbers()
-        {
+        public static int IsNumbers()        // creates a loop to determine if the input for age is all numbers. If not, message prints and loop begins again to get new input. 
+        {                                   // Then returns the age to GetAge() method.
             bool isNumbers;
             int age;
             do
             {
                 Console.WriteLine("How old are you?");
                 isNumbers = int.TryParse(Console.ReadLine(), out age);
-                if (isNumbers == false)
+                if (isNumbers == false)                                    //if invalid input is entered, message shows and loop begins again until a provided path is chosen.
                 {
                     Console.WriteLine("*facepalm* That...that isn't a valid age. Please make it easy on me by entering your age in whole-digit format.");
                     Console.WriteLine();
                 }
             } while (isNumbers == false);
-            return age;
+            return age;        //once parameters of only numbers is met, age is converted to an int and returned to the GetAge() method.
         }
 
-        public static string GetPronoun(string gender)
-        {
-            var pronoun = "they";
+        public static string GetPronoun(string gender)   //creates a switch case for the pronoun based on the gender chosen.
+        {                                               // If "other" was chosen as gender, message prints and asks for desired pronoun as input. 
+            var pronoun = "they";                       // Then returns the pronoun to CreateNewHuman() method. 
             switch (gender)
             {
                 case "other":
@@ -202,7 +202,7 @@ namespace MedievalTextGame
                     return pronoun;
             }
 
-            return pronoun;
+            return pronoun;   //just here to satisfy the return requirements for the method. This line will never get used. All returns can only come from within the do/while loop.
 
         }
        
--- MedievalTextGame/Program.cs
+++ MedievalTextGame/Program.cs
@@ -8,7 +8,7 @@ namespace MedievalTextGame
         {
             Console.WriteLine("Welcome to MEDIEVAL YOU");
             Console.WriteLine();
-            Human player1 = GetRace();
+            Human player1 = GetRace(); //creates new character
 
 
 
@@ -27,30 +27,30 @@ namespace MedievalTextGame
                 Console.WriteLine();
                 if (race == "human")
                 {
-                    Human player1 = new Human(Human.CreateNewHuman());
+                    Human player1 = new Human(Human.CreateNewHuman()); //calls method that will populate all Human properties.
                     return player1;
                 }
-                else if (race == "elf")
+                else if (race == "elf") //elves are not yet available in the game.
                 {
                     Console.WriteLine($"Hahahahahaha! Silly, there are no such things as Elves. You are a human.");
                     Console.WriteLine();
-                    Human player1 = new Human(Human.CreateNewHuman());
+                    Human player1 = new Human(Human.CreateNewHuman()); //calls Human class method that will populate all Human properties.
                     return player1;
                 }
-                else if (race == "goblin")
+                else if (race == "goblin") //goblins are not yet available in the game.
                 {
                     Console.WriteLine($"Hahahahahaha! Silly, there are no such things as {race}s. You are a human.");
                     Console.WriteLine();
-                    Human player1 = new Human(Human.CreateNewHuman());
+                    Human player1 = new Human(Human.CreateNewHuman()); //calls Human class method that will populate all Human properties.
                     return player1;
                 }
                 else
                 {
-                    Console.WriteLine($"Naughty, naughty...{race} was not one of the options shown.");
+                    Console.WriteLine($"Naughty, naughty...{race} was not one of the options shown."); //shows this message then restarts the loop until player chooses one of the designated races.
                     Console.WriteLine();
                 }
             } while (race != "human" && race != "elf" && race != "goblin");
-            Human player2 = new Human();
+            Human player2 = new Human(); //just here to satisfy the return requirements for the method. These two lines will never get used. All returns can only come from within the do/while loop.
             return player2;
         }
     }
